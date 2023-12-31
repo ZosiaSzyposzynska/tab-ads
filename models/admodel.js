ab459845294e08757d01b293148503dc23fbbf7a
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-12-31 15:41:11 +0100

    Add authMiddleware and auth routes

---
+++ models/admodel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const adSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 50,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 20,
+    maxlength: 1000,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  image: {
+    type: String, 
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Ad = mongoose.model('Ad', adSchema);
+
+module.exports = Ad;
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const adsRoutes = require('./routes/ads.routes');
+const authRoutes = require('./routes/auth.routes');
+const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://0.0.0.0:27017/ads', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
+
+app.use(
+  session({
+    secret: 'xyz567',
+    store: MongoStore.create(mongoose.connection),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  })
+);
+
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use('/api', adsRoutes);
+app.use('/auth', authRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
+});
