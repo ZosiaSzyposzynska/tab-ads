8cc06799a4f6dc6c25b9d9a94cffdce7386da13c
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2024-01-05 18:00:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(
 
 
 app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/api', adsRoutes);
 app.use('/auth', authRoutes);
 
